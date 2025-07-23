f4c8135c7eeab120014ba9bc13f885cf05508b61
mehak-glitch <mehakmeena515@gmail.com>
2025-07-23 17:20:42 +0530

    verification.js

---
+++ verification.js
@@ -0,0 +1,4 @@
+// This script will handle the "Continue" button and redirect to the main page (index.html)
+document.getElementById("continue-button").addEventListener("click", function() {
+    window.location.href = "index.html"; // Redirect to the main page
+});
